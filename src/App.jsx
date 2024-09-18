db18dd663e44468782e7fde292acb8cbc44a2d1e
Sergio Rivera <sergiorivfer@gmail.com>
2024-09-17 20:09:38 -0500

    My first componets

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { MyFirstComponents } from './componets/MyFirstComponents'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>
+        Fundamentos de React
+      </h1>
+      <MyFirstComponents/>
+    </>
+  )
+};
+
+export default App
---
+++ src/componets/MyFirstComponents.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const MyFirstComponents = () => {
+  return (
+    <>
+    <div>MyFirstComponents</div>
+    <h1> Temas de react</h1>
+    <ul>
+        <li>componentes</li>
+        <li>Eventos</li>
+        <li>estados Hoold</li>
+        <li>Promps</li>
+    </ul>
+    </> //fragment 
+  )
+}
